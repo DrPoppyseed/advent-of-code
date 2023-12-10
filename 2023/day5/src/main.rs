4ec20346ab12d7d5dd3d936359769f6d9d58f892
DrPoppyseed <peaske16180@gmail.com>
2023-12-11 01:19:28 +0900

    wip

--- 2023/day5/src/main.rs
+++ 2023/day5/src/main.rs
@@ -1,20 +1,19 @@
-use rayon::prelude::*;
 use std::{convert::Infallible, fs, str::FromStr};
 
 fn main() {
-    // println!("problem1: {}", problem1("inputs/2.txt"));
-    println!("problem2: {}", problem2("inputs/2.txt"));
+    println!("problem1: {}", problem1("inputs/1.txt"));
+    println!("problem2: {}", problem2("inputs/1.txt"));
 }
 
 #[derive(Default, Debug, Clone)]
-struct Rule {
+struct Mapping {
     source: usize,
     dest: usize,
     length: usize,
 }
 
-impl Rule {
-    fn apply_rule(&self, num: usize) -> Option<usize> {
+impl Mapping {
+    fn apply(&self, num: usize) -> Option<usize> {
         if num >= self.source && num <= (self.source + self.length) {
             let diff = num - self.source;
             Some(self.dest + diff)
@@ -24,7 +23,7 @@ impl Rule {
     }
 }
 
-impl FromStr for Rule {
+impl FromStr for Mapping {
     type Err = Infallible;
 
     fn from_str(val: &str) -> Result<Self, Self::Err> {
@@ -32,7 +31,7 @@ impl FromStr for Rule {
             unreachable!()
         };
 
-        Ok(Rule {
+        Ok(Mapping {
             source,
             dest,
             length,
@@ -40,17 +39,15 @@ impl FromStr for Rule {
     }
 }
 
-// This or the `name` could be an enum, but I'll keep it as a String until I learn
-// more about problem2
 #[derive(Default, Debug, Clone)]
 struct AlmanacMap {
-    rules: Vec<Rule>,
+    rules: Vec<Mapping>,
 }
 
 impl AlmanacMap {
     fn map(&self, num: usize) -> usize {
         for rule in &self.rules {
-            if let Some(location) = rule.apply_rule(num) {
+            if let Some(location) = rule.apply(num) {
                 return location;
             }
         }
@@ -78,7 +75,7 @@ fn make_maps(path: &str) -> (Vec<usize>, Vec<AlmanacMap>) {
         if line.contains("map") {
             maps.push(AlmanacMap::default())
         } else if !line.is_empty() {
-            let rule = Rule::from_str(&line).unwrap();
+            let rule = Mapping::from_str(&line).unwrap();
             let last_idx = maps.len() - 1;
             maps[last_idx].rules.push(rule);
         } else {
@@ -87,12 +84,6 @@ fn make_maps(path: &str) -> (Vec<usize>, Vec<AlmanacMap>) {
     }
 
     (seeds, maps)
-}
-
-#[derive(Debug)]
-struct SeedPair {
-    floor: usize,
-    range: usize,
 }
 
 fn problem1(path: &str) -> usize {
@@ -110,40 +101,123 @@ fn problem1(path: &str) -> usize {
     locations.into_iter().min().unwrap()
 }
 
-fn make_seed_pairs(nums: Vec<usize>) -> Vec<SeedPair> {
-    let mut pairs: Vec<SeedPair> = vec![];
-    for (i, num) in nums.iter().step_by(2).enumerate() {
-        pairs.push(SeedPair {
-            floor: *num,
-            range: nums[i * 2 + 1],
-        })
+// note to self: from day5 on, try to think of problem1 and problem2 as
+// completely different beasts. if you naievely try to reuse the same algorithm
+// from problem1 for problem2, you'll see the same kind of horrors I saw with
+// one.
+
+#[derive(Debug)]
+struct Remap {
+    remap_left: usize,
+    remap_right: usize,
+    remap_diff: i32,
+}
+
+impl From<String> for Remap {
+    fn from(val: String) -> Self {
+        let [dest, source, length] = line_to_nums(&val)[..3] else {
+            unreachable!()
+        };
+
+        Self {
+            remap_left: source,
+            remap_right: source + length - 1,
+            remap_diff: dest as i32 - source as i32,
+        }
     }
-    pairs
 }
 
 fn problem2(path: &str) -> usize {
-    let (nums, maps) = make_maps(path);
-    let sps = make_seed_pairs(nums);
-
-    sps.into_par_iter()
-        .map(|sp| {
-            let min = (sp.floor..sp.floor + sp.range + 1)
-                .into_par_iter()
-                .map(|seed| {
-                    let mut location: usize = seed;
-                    maps.clone().iter().for_each(|map| {
-                        location = map.map(location);
-                    });
-
-                    location
-                })
-                .min()
-                .unwrap();
-
-            min
-        })
-        .min()
+    let mut lines = fs::read_to_string(path)
         .unwrap()
+        .lines()
+        .map(String::from)
+        .collect::<Vec<_>>()
+        .into_iter();
+
+    let seeds = line_to_nums(&lines.next().unwrap());
+    let mut seeds = (0..seeds.len())
+        .into_iter()
+        .step_by(2)
+        .map(|i| (seeds[i], seeds[i] + seeds[i + 1] - 1))
+        .collect::<Vec<(usize, usize)>>();
+
+    lines.next();
+
+    while let Some(line) = lines.next() {
+        if line.contains("map") || line.is_empty() {
+            continue;
+        } else {
+            let Remap {
+                remap_left,
+                remap_right,
+                remap_diff,
+            } = Remap::from(line.clone());
+
+            // find out if mapping overlaps with any of the seeds
+            // if mapping overlaps with any, update for range
+            let mut remapped_seeds: Vec<(usize, usize)> = vec![];
+            while !seeds.is_empty() {
+                let (left, right) = seeds.pop().unwrap();
+                if left <= remap_right && right >= remap_left {
+                    // complete overlap. numbers in [max(remap_left, seed_left),
+                    // min(remap_right, seed_right)] will be remapped
+                    if left >= remap_left && right <= remap_right {
+                        // seed range is inside remap range
+                        println!("{left}-{right} inside");
+                        remapped_seeds.push((
+                            (left as i32 + remap_diff) as usize,
+                            (right as i32 + remap_diff) as usize,
+                        ));
+                    } else if left <= remap_right && right >= remap_right {
+                        // overlaps partially on seed's right
+                        // append portion that overlaps (left, remap_right)
+                        println!("{left}-{right} right");
+                        remapped_seeds.push((
+                            (left as i32 + remap_diff) as usize,
+                            (remap_right as i32 + remap_diff) as usize,
+                        ));
+
+                        // append portion that doesn't overlap (remap_right, right)
+                        remapped_seeds.push((remap_right + 1, right));
+                    } else if left <= remap_left && right >= remap_left {
+                        // overlaps partially on seed's left
+                        // append portion that overlaps (left, remap_left)
+                        println!("{left}-{right} left");
+                        remapped_seeds.push((
+                            (left as i32 + remap_diff) as usize,
+                            (remap_left as i32 + remap_diff) as usize,
+                        ));
+
+                        // append portion that doens't overlap (remap_left, right)
+                        remapped_seeds.push((remap_left + 1, right));
+                    } else {
+                        // overlaps partially on both sides
+                        // append portion that doesn't overlap on left (left, remap_left)
+                        println!("{left}-{right}) both");
+                        remapped_seeds.push((left, remap_left));
+
+                        // append portion that overlaps
+                        remapped_seeds.push((
+                            (remap_left as i32 + 1 + remap_diff) as usize,
+                            (remap_right as i32 + remap_diff) as usize,
+                        ));
+
+                        // append portion that doesn't overlap on right (remap_right, right)
+                        remapped_seeds.push((remap_right + 1, right));
+                    }
+                } else {
+                    remapped_seeds.push((left, right));
+                }
+            }
+
+            seeds.append(&mut remapped_seeds);
+            seeds.sort();
+            println!("remap: {remap_left}-{remap_right}, {remap_diff}   \t| seed: {seeds:?}");
+        }
+    }
+
+    seeds.into_iter().map(|(left, _)| left).min().unwrap()
 }
 
 #[cfg(test)]
